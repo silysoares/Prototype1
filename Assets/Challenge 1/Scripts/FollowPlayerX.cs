37865e197d747049aa69164bfc99b67ebc03e996
silysoares <104627031+silysoares@users.noreply.github.com>
2023-04-24 22:09:03 +0100

    5. O avião está se inclinando automaticamente

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -6,6 +6,7 @@ public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     private Vector3 offset;
+    private Vector3 offset = new Vector3(30,0,10);
 
     // Start is called before the first frame update
     void Start()
