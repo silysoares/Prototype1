32402ab3f80809a8031ed8f481c73a31b3d032c7
silysoares <104627031+silysoares@users.noreply.github.com>
2023-04-24 16:05:15 +0100

    3. O avião está indo para trás

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    private float speed = 2000.0f;
+    private float turnSpeed = 450.0f;
     private float horizontalInput;
     private float forwardInput;
     // Start is called before the first frame update
@@ -26,7 +26,7 @@ public class PlayerController : MonoBehaviour
        //Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
        
     }
 }                                                      
